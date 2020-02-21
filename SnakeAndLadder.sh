754ecaa1d323083fc7d5ec694676141fd1b5279f
akshayakky <akshaysmpt01@gmail.com>
2020-02-21 11:56:04 +0530

    ADDED : Added Actions To Be Performed And Perform Particular Action

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -4,6 +4,24 @@ echo Welcome To Snake And Ladder Simulation
 
 #CONSTANTS
 START_POSITION=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
 
 #VARIABLES
 dieRoll=$((RANDOM%6+1))
+action=$((RANDOM%3))
+pos=$START_POSITION
+
+#CHECKING FOR ACTION TO BE PERFORMED AND PERFORMING IT
+case $action in 
+	$NO_PLAY)
+		pos=$pos
+		;;
+	$LADDER)
+		pos=$(($pos+$dieRoll))
+		;;
+	$SNAKE)
+		pos=$(($pos-$dieRoll))
+		;;
+esac
